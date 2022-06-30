368b7d691a019ad2fc408732e277adcb57827196
Emran12 <emran6452@gmail.com>
2022-06-30 23:02:40 +0600

    get api added for getting all task

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const express = require("express");
-
 const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://user:1234@cluster0.wc8jp.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const taskCollection = client.db("todo").collection("task");
+    app.get("/task", async (req, res) => {
+      const query = {};
+      const cursor = taskCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", async (req, res) => {
   res.send("Hello World!");
